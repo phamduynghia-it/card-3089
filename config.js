3acd20d3e6a5a50cf8546b7f4df38b98d6592db5
phamduynghia-it <162310524+phamduynghia-it@users.noreply.github.com>
2026-07-15 17:20:59 +0700

    Deploy greeting card with dynamic assets and custom workflow

---
+++ config.js
@@ -0,0 +1,4 @@
+const texts = ["Em là cả thế giới.","Anh yêuu emm Như Ngọc","Anh vẫn chờ em nhé.","Yêu em đến trọn đời.","Mình về bên nhau nhé."];
+
+// Đường dẫn nhạc, bạn có thể thay bằng link mp3 hoặc file mp3 có sẵn trong máy
+const musicPath = "./assets/user_bg_music.mp3";
